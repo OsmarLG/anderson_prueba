dccca5848fe71c850b6b9364d9e3c6aa84c84ef4
osmarliera <osmar2001lg@gmail.com>
2024-03-02 02:10:56 -0700

    last commit v2

--- app/views/register/php/validate.php
+++ app/views/register/php/validate.php
@@ -20,6 +20,15 @@
         try {
             $db = new PDO("mysql:host=$host;dbname=$dbname", $db_username, $db_password);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $checkUser = $db->prepare("SELECT * FROM users WHERE username = :username");
+            $checkUser->execute([':username' => $username]);
+            $userExists = $checkUser->fetch(PDO::FETCH_ASSOC);
+
+            if ($userExists) {
+                echo json_encode(['success' => false, 'error' => 'El nombre de usuario ya está en uso.']);
+                exit();
+            }
         
             $stmt = $db->prepare("INSERT INTO users (name, username, password, rol) VALUES (:name, :username, :password, :rol)");
             $hashed_password = password_hash($password, PASSWORD_DEFAULT);
